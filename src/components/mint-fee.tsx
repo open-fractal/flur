eae4e4f8ffd009085bc75fbb0bb46742d617a85f
utxo detective <harrison@beckerich.org>
2024-09-19 11:24:30 -0500

    remove api url

--- src/components/mint-fee.tsx
+++ src/components/mint-fee.tsx
@@ -1,22 +1,17 @@
 import React from 'react'
-import useSWR from 'swr'
-import { API_URL } from '@/lib/constants'
+// import useSWR from 'swr'
+// import { API_URL } from '@/lib/constants'
 
 interface MintFeeProps {
-  className?: string
+	className?: string
 }
 
-const fetcher = (url: string) => fetch(url).then(res => res.json())
+// const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export const MintFee: React.FC<MintFeeProps> = ({ className }) => {
-  const { data: mintFeeData, error } = useSWR(`${API_URL}/api/mint-fee`, fetcher)
+	// const { data: mintFeeData, error } = useSWR(`${API_URL}/api/mint-fee`, fetcher)
+	// if (!mintFeeData) return <div className={`${className} text-gray-500`}>Loading...</div>
+	// if (error) return <div className={`${className} text-gray-500`}>Error fetching mint fee</div>
 
-  if (!mintFeeData) return <div className={`${className} text-gray-500`}>Loading...</div>
-  if (error) return <div className={`${className} text-gray-500`}>Error fetching mint fee</div>
-
-  return (
-    <div className={`${className} text-gray-400`}>
-      Service Fee: {mintFeeData.fee} 0 Sats
-    </div>
-  )
+	return <div className={`${className} text-gray-400`}>Service Fee: 0 Sats</div>
 }
