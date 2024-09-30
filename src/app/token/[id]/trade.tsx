a908363ee539e9f3dce5d73e3960c1f4ef6339cb
utxo detective <harrison@beckerich.org>
2024-09-29 21:46:59 -0500

    updates

--- src/app/token/[id]/trade.tsx
+++ src/app/token/[id]/trade.tsx
@@ -17,16 +17,19 @@ interface TradeProps {
 const Trade: React.FC<TradeProps> = ({ token, showMarket }) => {
 	const { width, height, ref } = useComponentSize()
 
-	return (
-		<div className="W-full h-full flex flex-grow">
-			<div className="flex-grow flex flex-col items-center justify-center">
-				<div className="flex flex-col justify-center items-center">
-					<h2 className="text-2xl font-bold mb-4">Mint Ended</h2>
-					<p className="text-muted-foreground">Check back later!</p>
+	if (!showMarket) {
+		return (
+			<div className="W-full h-full flex flex-grow">
+				<div className="flex-grow flex flex-col items-center justify-center">
+					<div className="flex flex-col justify-center items-center">
+						<h2 className="text-2xl font-bold mb-4">Mint Ended</h2>
+						<p className="text-muted-foreground">Check back later!</p>
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
+
 	return (
 		<div className="W-full h-full flex flex-grow">
 			<div className="flex-grow flex flex-col">
